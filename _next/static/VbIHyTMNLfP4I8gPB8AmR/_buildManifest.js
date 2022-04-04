52f4ca62d718cb3315b7b5df20620941bd77623a
Kerrex <romen3@gmail.com>
2022-04-04 23:18:12 +0200

    Initial commit

---
+++ _next/static/VbIHyTMNLfP4I8gPB8AmR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0ddf4930f12e0914.css","static/chunks/pages/index-d74d6a76a0b90991.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
