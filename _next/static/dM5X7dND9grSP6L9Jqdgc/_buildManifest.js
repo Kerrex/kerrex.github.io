fd74307cc410fa1d6c1990d8d065b189610203b9
Kerrex <romen3@gmail.com>
2022-08-22 20:34:32 +0200

    Updated website

---
+++ _next/static/dM5X7dND9grSP6L9Jqdgc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/931-c25abbdf499c0f5c.js","static/css/47005327eebef672.css","static/chunks/pages/index-d882623c8aa4c70c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
