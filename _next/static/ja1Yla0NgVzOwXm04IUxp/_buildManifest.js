dade21014ef1cb794090d4ab0a3fc4ff41ce1ff5
Kerrex <romen3@gmail.com>
2024-04-07 19:44:07 +0200

    Update prices

---
+++ _next/static/ja1Yla0NgVzOwXm04IUxp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/931-c25abbdf499c0f5c.js","static/css/0544f611d5b8c44f.css","static/chunks/pages/index-ea9cd3deccc122d6.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
