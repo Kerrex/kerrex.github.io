48c09fa35b9e2a039875d0b66caff53e3b5b8737
Tomasz Morawski <romen3@gmail.com>
2025-02-09 17:36:26 +0100

    Change prices (old website)

---
+++ _next/static/uIi9ALHAEni1qFp2xa_0L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/182-d83b97577b9e5cbc.js","static/css/0544f611d5b8c44f.css","static/chunks/pages/index-b1638f3dbf394d50.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
